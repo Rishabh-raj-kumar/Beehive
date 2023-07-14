acf1a0470c732dfdbbaf8d0c091d114b91840225
Rishabh <108469021+Rishabh-raj-kumar@users.noreply.github.com>
2023-07-15 00:07:06 +0530

    initial

---
+++ src/firebase/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp} from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import 'firebase/firestore';
+import { Firestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHJ4QosAFGXvi0LpxsZKlrXk701pr3a58",
+  authDomain: "instagram-a42d4.firebaseapp.com",
+  projectId: "instagram-a42d4",
+  storageBucket: "instagram-a42d4.appspot.com",
+  messagingSenderId: "54173696537",
+  appId: "1:54173696537:web:7c679af888d5affa18e088",
+  measurementId: "G-FKZSHBKMME"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebase);
+const FieldValue = Firestore;
+
+export {firebase, FieldValue};
