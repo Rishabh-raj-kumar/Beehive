5064aefba1982bd4cd9aa5f985e99e6203ba7223
Rishabh Raj Kumar <108469021+Rishabh-raj-kumar@users.noreply.github.com>
2023-10-26 18:20:12 +0530

    Update News.js

--- src/Components/sidebar/News.js
+++ src/Components/sidebar/News.js
@@ -1,35 +1,35 @@
 import React, { useEffect, useState } from "react";
 
 function News() {
-  const [items, setItems] = useState([]);
-  const [category, setCategory] = useState("general");
+  // const [items, setItems] = useState([]);
+  // const [category, setCategory] = useState("general");
 
-  useEffect(() => {
-    try {
-      fetch(
-        `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=c10d9cfbadf0469cb2f174003492eb3e`
-      )
-        .then((res) => res.json())
-        .then((data) => setItems(data.articles));
-    } catch (err) {
-      console.log(err);
-    }
-  }, [category]);
-  return (
-    <div className=" bg-white rounded-lg">
-      <h2 className=" font-medium text-xl p-3">Latest News</h2>
-      <div className=" h-96 overflow-y-scroll">
-        <div className=" flex flex-col gap-2 overflow-y-scroll">
-          {items.map((x) => (
-            <div className=" flex-1 bg-white rounded shadow-md p-2">
-              <img src={x.urlToImage} className=" w-full h-28" />
-              <h2 className=" text-lg font-medium">{x.title}</h2>
-              <p>{x.description}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
+  // useEffect(() => {
+  //   try {
+  //     fetch(
+  //       `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=c10d9cfbadf0469cb2f174003492eb3e`
+  //     )
+  //       .then((res) => res.json())
+  //       .then((data) => setItems(data.articles));
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // }, [category]);
+  // return (
+  //   <div className=" bg-white rounded-lg">
+  //     <h2 className=" font-medium text-xl p-3">Latest News</h2>
+  //     <div className=" h-96 overflow-y-scroll">
+  //       <div className=" flex flex-col gap-2 overflow-y-scroll">
+  //         {items.map((x) => (
+  //           <div className=" flex-1 bg-white rounded shadow-md p-2">
+  //             <img src={x.urlToImage} className=" w-full h-28" />
+  //             <h2 className=" text-lg font-medium">{x.title}</h2>
+  //             <p>{x.description}</p>
+  //           </div>
+  //         ))}
+  //       </div>
+  //     </div>
+  //   </div>
   );
 }
 
